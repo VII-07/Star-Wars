1406fc5464569f3ebaeaf30f3fb156e652357c68
VII-07 <masterb21052017@gmail.com>
2022-04-13 16:28:39 +0300

    Switch

--- script.js
+++ script.js
@@ -16,33 +16,48 @@ fetch('https://swapi.dev/api/people')
 
         persons.forEach(people => {
         
-            if(people.hair_color == 'blond'){
-                people.hair_color ='yellow';
+            // if(people.hair_color == 'blond'){
+            //     people.hair_color ='yellow';
+            // }
+            // if(people.skin_color == 'fair'){
+            //     people.skin_color ='red';
+            // }
+            // if(people.skin_color == 'light'){
+            //     people.skin_color ='white';
+            // }
+            // if(people.skin_color == 'white, blue'){
+            //     people.skin_color ='blue';
+            // }
+            // if(people.skin_color == 'fair'){
+            //     people.skin_color ='red';
+            // }
+            // if(people.hair_color == 'brown, grey'){
+            //     people.hair_color ='brown';
+            // }
+            // if(people.skin_color == 'white, red'){
+            //     people.skin_color ='white';
+            // }
+            // if(people.hair_color == 'auburn, white'){
+            //     people.hair_color ='white';
+            // }
+            // if(people.eye_color == 'blue-gray'){
+            //     people.eye_color ='blue';
+            // }
+            switch(people.hair_color){
+                case 'blond': people.hair_color ='yellow'; break;
+                case 'brown, grey': people.hair_color ='brown'; break;
+                case 'auburn, white': people.hair_color ='white'; break;
             }
-            if(people.skin_color == 'fair'){
-                people.skin_color ='red';
+            switch(people.skin_color) {
+                case 'fair': people.skin_color ='red'; break;
+                case 'light': people.skin_color ='white'; break;
+                case 'white, blue': people.skin_color ='blue'; break;
+                case 'white, red': people.skin_color ='white'; break;
             }
-            if(people.skin_color == 'light'){
-                people.skin_color ='white';
+            switch(people.eye_color) {
+                case 'blue-gray': people.eye_color ='blue'; break;
             }
-            if(people.skin_color == 'white, blue'){
-                people.skin_color ='blue';
-            }
-            if(people.skin_color == 'fair'){
-                people.skin_color ='red';
-            }
-            if(people.hair_color == 'brown, grey'){
-                people.hair_color ='brown';
-            }
-            if(people.skin_color == 'white, red'){
-                people.skin_color ='white';
-            }
-            if(people.hair_color == 'auburn, white'){
-                people.hair_color ='white';
-            }
-            if(people.eye_color == 'blue-gray'){
-                people.eye_color ='blue';
-            }
+
         })
 
         cardName.innerHTML = persons[count].name;
@@ -72,32 +87,46 @@ next.addEventListener('click', async() => {
 
     persons.forEach(people => {
         
-        if(people.hair_color == 'blond'){
-            people.hair_color ='yellow';
+        // if(people.hair_color == 'blond'){
+        //     people.hair_color ='yellow';
+        // }
+        // if(people.skin_color == 'fair'){
+        //     people.skin_color ='red';
+        // }
+        // if(people.skin_color == 'light'){
+        //     people.skin_color ='white';
+        // }
+        // if(people.skin_color == 'white, blue'){
+        //     people.skin_color ='blue';
+        // }
+        // if(people.skin_color == 'fair'){
+        //     people.skin_color ='red';
+        // }
+        // if(people.hair_color == 'brown, grey'){
+        //     people.hair_color ='brown';
+        // }
+        // if(people.skin_color == 'white, red'){
+        //     people.skin_color ='white';
+        // }
+        // if(people.hair_color == 'auburn, white'){
+        //     people.hair_color ='white';
+        // }
+        // if(people.eye_color == 'blue-gray'){
+        //     people.eye_color ='blue';
+        // }
+        switch(people.hair_color){
+            case 'blond': people.hair_color ='yellow'; break;
+            case 'brown, grey': people.hair_color ='brown'; break;
+            case 'auburn, white': people.hair_color ='white'; break;
         }
-        if(people.skin_color == 'fair'){
-            people.skin_color ='red';
+        switch(people.skin_color) {
+            case 'fair': people.skin_color ='red'; break;
+            case 'light': people.skin_color ='white'; break;
+            case 'white, blue': people.skin_color ='blue'; break;
+            case 'white, red': people.skin_color ='white'; break;
         }
-        if(people.skin_color == 'light'){
-            people.skin_color ='white';
-        }
-        if(people.skin_color == 'white, blue'){
-            people.skin_color ='blue';
-        }
-        if(people.skin_color == 'fair'){
-            people.skin_color ='red';
-        }
-        if(people.hair_color == 'brown, grey'){
-            people.hair_color ='brown';
-        }
-        if(people.skin_color == 'white, red'){
-            people.skin_color ='white';
-        }
-        if(people.hair_color == 'auburn, white'){
-            people.hair_color ='white';
-        }
-        if(people.eye_color == 'blue-gray'){
-            people.eye_color ='blue';
+        switch(people.eye_color) {
+            case 'blue-gray': people.eye_color ='blue'; break;
         }
     })
 
